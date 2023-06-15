5e8635cf7768da46a9b68ca0a7a21a17e0ee197c
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2023-06-15 15:43:57 -0500

    edit CorsConfiguration

--- employee-management/src/main/java/com/developer/employeemanagement/controller/EmployeeController.java
+++ employee-management/src/main/java/com/developer/employeemanagement/controller/EmployeeController.java
@@ -15,6 +15,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 
     private final RestTemplate restTemplate;
