d66667588e3410d2facadc1e6a19f9c8120f916c
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2023-06-15 15:59:16 -0500

    edit CorsConfiguration

--- employee-management/src/main/java/com/developer/employeemanagement/EmployeeManagementSystemApplication.java
+++ employee-management/src/main/java/com/developer/employeemanagement/EmployeeManagementSystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,14 +17,15 @@ public class EmployeeManagementSystemApplication {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer configure() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
-			}
-		};
+	@Configuration
+	public class MyConfiguration implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+					.allowedMethods("*");
+		}
+
 	}
 
 }
